d63a4f1987c98808d8eb0132000a3822761258df
Mirko965 <mirkojelic.jelic@gmail.com>
2019-04-13 12:05:23 +0200

    serv html page

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
 
-app.get('/',(req,res) => {
+/*app.get('/',(req,res) => {
   res.send('Hello from ubuntu-node-aws')
-})
+})*/
+app.use(express.static('public'))
 
 app.listen(3000,() => {
   console.log('server listen on port 3000')
